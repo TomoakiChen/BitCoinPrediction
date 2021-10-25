a4b96d833d315c8a505a5c81b44be9ffd2e2ca38
TomoakiChen <archerugly@gmail.com>
2021-10-26 00:57:01 +0800

    準備新增 Yahoo新聞的 爬蟲
    Yahoo News API 似乎已經不再提供

--- NewsCrawler.py
+++ NewsCrawler.py
@@ -49,6 +49,48 @@ class LTNClient(HtmlClient):
             news_info_list.append(news_info)
         return news_info_list
 
+
+class YahooNewsClient(HtmlClient):
+
+    def __init__(self, news_since=datetime.now().date(), news_until=datetime.now().date()):
+        super().__init__()
+        self._url_pattern = 'https://tw.news.yahoo.com/tag/比特幣'
+        self._news_since = news_since
+        self._news_until = news_until
+
+    def getNewsInfoList(self):
+        news_info_list = []
+        while self._now_page <= self._max_pages:
+            #self._miningOnePage(news_info_list)
+            news_info_list = self._miningOnePage(news_info_list)
+        return news_info_list
+
+    def _doSetupNowUrl(self):
+        self._now_url = self._url_pattern.replace('${PAGE}', str(self._now_page) )
+        #print(self._now_url)
+        self._now_page += 1
+
+    #def _doMiningOnePage(self):
+    def _miningOnePage(self, news_info_list):
+        self._doSetupNowUrl()
+        html_parsed_data = self.getHtml(self._now_url)
+
+        # data-desc='新聞列表' > class='searchlist' > li
+        news_list = html_parsed_data.find(class_='searchlist').findAll('li') #新聞(標題)清單
+        for news in news_list:
+            news_info = NewsInfo()
+            links = news.findAll('a')
+            #print(links)
+
+            #print(links[1].href).setTitle())
+            news_info.setLink(links[1]['href']) #新聞連結
+            news_info.setTitle(links[1].find('h3').getText()) #新聞標題
+            # print(news_info)
+
+            news_info_list.append(news_info)
+        return news_info_list
+
+
 # --------------------------------- 以下是 自由時報格式 ---------------------------------
 ltn_client = LTNClient()
 
