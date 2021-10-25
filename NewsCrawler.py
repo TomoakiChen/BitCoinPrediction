55d82d9e8f69d8b49d0f96949b3478b4dd5d0fbb
TomoakiChen <archerugly@gmail.com>
2021-10-26 07:48:55 +0800

    Update NewsCrawler.py

--- NewsCrawler.py
+++ NewsCrawler.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
 from PyWeb import HtmlClient
 from NewsInfo import NewsInfo
-class LTNClient:
-    def __init__(self, html_client = None):
-        self._html_client = html_client
-    def _setupHtmlClient(html_client):
-        self._html_client = HtmlClient()
 # --------------------------------- 以下是 自由時報格式 ---------------------------------
 news_info_list = []
 url = 'https://news.ltn.com.tw/topic/%E6%AF%94%E7%89%B9%E5%B9%A3'
-if(html)
 html_client = HtmlClient()
 html_parsed_data = html_client.getHtml(url)
 #print(parsed_data)
@@ -26,3 +20,16 @@ for news in news_list:
     news_info.setTitle(links[1].find('h3').getText()) #新聞標題
     print(news_info)
 # --------------------------------- 以上是 自由時報格式 ---------------------------------
+
+
+news_info_list = []
+url = 'https://tw.news.yahoo.com/tag/%E6%AF%94%E7%89%B9%E5%B9%A3'
+html_client = HtmlClient()
+html_parsed_data = html_client.getHtml(url)
+
+# ul[class="stream-container-scroll-template"] > li[class=""StreamMegaItem]
+# StreamMegaItem
+
+news_list = html_parsed_data.findAll(class_='StreamMegaItem')
+print(len(news_list))
+# https://edge-mcdn.secure.yahoo.com/dms/exp.json
