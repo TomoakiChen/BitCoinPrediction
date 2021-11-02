e5d0e7b73b81f0e9147841ce696fe8f03cc452be
TomoakiChenSinica <tomoaki.sinica@gmail.com>
2021-11-02 14:34:19 +0800

    新增 鉅亨網、經濟日報

--- NewsCrawler.py
+++ NewsCrawler.py
@@ -8,6 +8,12 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
+class NewsInfoHelper:
+    @staticmethod
+    def filterBySincaDate(ori_news_info_list, since_date):
+        since_datetime = datetime.combine(since_date, datetime.min.time())
+        filtered_news_info_list = [news_info for news_info in ori_news_info_list if news_info.getPubDateTime() >= since_datetime]
+        return filtered_news_info_list
 
 class LTNNewsClient(HtmlClient):
 
@@ -152,4 +158,137 @@ class YahooNewsClient(WebDriverClient):
 
         news_info.setTitle(text)
         news_info.setLink(link_url)
+        return news_info
+
+class cnYESNewsClient(WebDriverClient):
+
+    def __init__(self, url='https://www.cnyes.com/search/news?keyword=比特幣', headless=False):
+        super().__init__(headless=headless)
+        self.__url = url
+
+        self.__now_page = 0
+
+    def findByMaxPages(self, max_pages=10):
+        self.__doReadyLoadPage()
+        news_info_list = []
+        while self.__now_page <= max_pages:
+            #news_info_list = self.__miningOnePage(news_info_list)
+            news_info_list = self.__miningOnePage()
+        return news_info_list
+
+    def findBySinceDate(self, since_date=date.today()):
+        self.__doReadyLoadPage()
+        news_info_list = []
+        while True:
+            news_info_list = self.__miningOnePage()
+            filtered_news_info_list = NewsInfoHelper.filterBySincaDate(news_info_list, since_date)
+
+    def __doReadyLoadPage(self):
+        self._browser_driver.get(self.__url)
+        container_element = self._browser_driver.find_element(By.TAG_NAME, 'body')
+        actions = webdriver.ActionChains(self._browser_driver)
+        actions.click(container_element)
+        actions.perform()
+
+    def __miningOnePage(self):
+        actions = webdriver.ActionChains(self._browser_driver)
+        actions.key_down(Keys.END)
+        actions.pause(5)
+        # actions.perform()
+        parsed_data = self.getHtml(self.__url, action_chains=actions)
+        #print("parsed_data=", parsed_data)
+        self.__now_page += 1
+
+        news_info_list = []
+        # '[id="__SearchAll"]'(搜尋結果外框) > a[class="news"] (每一條新聞)
+        news_element_list = parsed_data.find(id="_SearchAll").findAll("a", class_='news')
+        for news_element in news_element_list:
+            news_info = self.__obtainNewsInfo(news_element)
+            news_info_list.append(news_info)
+        return news_info_list
+
+    def __obtainNewsInfo(self, news_element):
+        news_info = NewsInfo()
+        link_url = news_element["href"]
+
+        time_element = news_element.find(class_="cat_time").find("time")
+        str_pub_datetime = time_element["datetime"]
+        pub_datetime = datetime.fromisoformat(str_pub_datetime)
+        # print("pub_datetime = ", pub_datetime)
+
+        title_element = news_element.find(class_="news_title").find("h3")
+        title = title_element.getText()
+        # print("title = ", title)
+
+        news_info.setTitle(title)
+        news_info.setLink(link_url)
+        news_info.setPubDateTime(pub_datetime)
+        return news_info
+
+"""
+經濟日報
+"""
+class MoneyUdnNewsClient(WebDriverClient):
+
+    def __init__(self, url='https://money.udn.com/search/result/1001/比特幣', headless=False):
+        super().__init__(headless=headless)
+        self.__url = url
+
+        self.__now_page = 0
+
+    def findByMaxPages(self, max_pages=10):
+        self.__doReadyLoadPage()
+        news_info_list = []
+        while self.__now_page <= max_pages:
+            #news_info_list = self.__miningOnePage(news_info_list)
+            news_info_list = self.__miningOnePage()
+        return news_info_list
+
+    def findBySinceDate(self, since_date=date.today()):
+        self.__doReadyLoadPage()
+        news_info_list = []
+        while True:
+            news_info_list = self.__miningOnePage()
+            filtered_news_info_list = NewsInfoHelper.filterBySincaDate(news_info_list, since_date)
+
+    def __doReadyLoadPage(self):
+        self._browser_driver.get(self.__url)
+        container_element = self._browser_driver.find_element(By.TAG_NAME, 'body')
+        actions = webdriver.ActionChains(self._browser_driver)
+        actions.click(container_element)
+        actions.perform()
+
+    def __miningOnePage(self):
+        actions = webdriver.ActionChains(self._browser_driver)
+        actions.key_down(Keys.END)
+        actions.pause(5)
+        # actions.perform()
+        parsed_data = self.getHtml(self.__url, action_chains=actions)
+        #print("parsed_data=", parsed_data)
+        self.__now_page += 1
+
+        news_info_list = []
+        # '[id="__SearchAll"]'(搜尋結果外框) > a[class="news"] (每一條新聞)
+        news_element_list = parsed_data.find("ul", class_="story-list-holder").findAll("li", class_='story-headline-wrapper')
+        for news_element in news_element_list:
+            news_info = self.__obtainNewsInfo(news_element)
+            news_info_list.append(news_info)
+        return news_info_list
+
+    def __obtainNewsInfo(self, news_element):
+        news_info = NewsInfo()
+        story_content_element = news_element.find("div", class_="story__content")
+
+        time_element = story_content_element.find("time")
+        str_pub_datetime = time_element.getText()
+
+        title_element = story_content_element.find("h3", class_="story__headline")
+        title = title_element.getText().strip() # 此網頁會將搜尋的字眼化底線...(會多一個 <u></u>) --> 不曉得是不是自動處理掉了，反而是要處理前後空白
+
+        link_element = story_content_element.find("a")
+        link_url = link_element["href"]
+
+        news_info.setTitle(title)
+        news_info.setLink(link_url)
+        # news_info.setPubDateTime(pub_datetime)
         return news_info
--- NewsCrawlerDemo.py
+++ NewsCrawlerDemo.py
@@ -1,4 +1,4 @@
-from NewsCrawler import LTNNewsClient, YahooNewsClient
+from NewsCrawler import LTNNewsClient, YahooNewsClient, cnYESNewsClient, MoneyUdnNewsClient
 from datetime import datetime, date
 
 # --------------------------------- 以下是 自由時報格式 ---------------------------------
@@ -19,6 +19,8 @@ print("cost = ", (end - start))
 """
 # --------------------------------- 以上是 自由時報格式 ---------------------------------
 
+# --------------------------------- 以下是 Yahoo 新聞 ---------------------------------
+"""
 start = datetime.now()
 yahoo_news_client = YahooNewsClient(headless=True)
 
@@ -29,3 +31,36 @@ for news in news_list:
     print(news)
 print("total nums = ", len(news_list))
 print("cost = ", (end - start))
+"""
+# --------------------------------- 以上是 Yahoo 新聞 ---------------------------------
+
+
+# --------------------------------- 以下是 鉅亨網 ---------------------------------
+"""
+start = datetime.now()
+cnYES_news_client = cnYESNewsClient(headless=True)
+
+news_list = cnYES_news_client.findByMaxPages(max_pages=10)
+end = datetime.now()
+
+for news in news_list:
+    print(news)
+print("total nums = ", len(news_list))
+print("cost = ", (end - start))
+"""
+# --------------------------------- 以上是 鉅亨網 ---------------------------------
+
+
+
+# --------------------------------- 以下是 經濟日報 ---------------------------------
+start = datetime.now()
+money_udn_news_client = MoneyUdnNewsClient(headless=True)
+
+news_list = money_udn_news_client.findByMaxPages(max_pages=10)
+end = datetime.now()
+
+for news in news_list:
+    print(news)
+print("total nums = ", len(news_list))
+print("cost = ", (end - start))
+# --------------------------------- 以上是 經濟日報 ---------------------------------
