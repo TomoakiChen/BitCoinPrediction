912357001b0ab45ad6c0cc7c9244673749023d30
TomoakiChen <archerugly@gmail.com>
2021-11-13 10:11:25 +0800

    正式了解如何利用 DataFrame 、GroupBy

--- CoinPriceLibDemo2.py
+++ CoinPriceLibDemo2.py
@@ -1,16 +1,12 @@
-from CoinPriceLib import CryptoDatadownloadBinaceClient, CryptoDatadownloadBinaceCsvDataParser
 import pandas as pd
+from CoinPriceLib import CryptoDatadownloadBinaceClient, CryptoDatadownloadBinaceCsvDataParser
+from PandasHelper import PandasDataFrameHelper
 
 client = CryptoDatadownloadBinaceClient()
 
 
-hourly_df = pd.DataFrame({
-    "date" : ["2021-11-12 13:00:00", "2021-11-12 12:00", "2021-11-11 07:00", "2021-10-01 08:00"],
-    "close" : [10000, 20000, 30000, 100]
-})
-hourly_df["date"] = CryptoDatadownloadBinaceCsvDataParser.parseDateInfo(hourly_df["date"])
-
-#hourly_df = client.getHourlyDataFrame(desig_col_list=["date", "close"])
+# hourly_df = client.getHourlyDataFrame4Test()
+hourly_df = client.getHourlyDataFrame(desig_col_list=["date", "close"])
 
 #print(hourly_df)
 grouper = pd.Grouper(key="date", freq="7D")
@@ -20,8 +16,19 @@ grouped_hourly_df = hourly_df.groupby(grouper)
 
 groups = grouped_hourly_df.groups
 indices = grouped_hourly_df.indices
+
+"""
+# ==================================================== 以下印出 groups、indices 的屬性 ====================================================
 print(type(groups)) # dict
 print(type(indices)) # defaultdict
+
+print(groups.keys())
+print(groups.values())
+
+print(indices.keys())
+print(indices.values())
+# ==================================================== 以上印出 groups、indices 的屬性 ====================================================
+"""
 
 """
 for group, indice in zip(groups, indices):
@@ -38,8 +45,26 @@ for group in groups:
         break
 """
 
-print(groups.keys())
-print(groups.values())
 
-print(indices.keys())
-print(indices.values())
+"""
+group_names = groups.keys()
+print(type(group_names))
+
+for group_name in group_names:
+    # group_df = grouped_hourly_df[group_name]
+    group_df = grouped_hourly_df.get_group(group_name)
+    print(group_df)
+"""
+
+group_names = indices.keys()
+print(type(group_names))
+
+for group_name in group_names:
+    # group_df = grouped_hourly_df[group_name] 用錯成這個會顯示 object is not subscriptable
+    group_df = grouped_hourly_df.get_group(group_name)
+    print("type = " + str( type(group_df) ) )
+    print("size = " + str(group_df.size) )
+    print("shape = " + str(group_df.shape) )
+    print("Rows = " + str(PandasDataFrameHelper.analyzeNums4Rows(group_df)) )
+    print(group_df)
+    print("==============================================================")
