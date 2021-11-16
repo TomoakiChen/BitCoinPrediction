da83c193ee7f2302a8174f694caa5a240024f333
TomoakiChen <archerugly@gmail.com>
2021-11-16 21:49:06 +0800

    panda DataFrame 資料各種處理
    目前主要握 normalizaiton

--- PandasHelper.py
+++ PandasHelper.py
@@ -9,3 +9,19 @@ class PandasDataFrameHelper:
     @staticmethod
     def analyzeNums4Cols(df):
         return df.shape[1]
+
+    @staticmethod
+    def processDataNormalizationByMean(df, desig_col_list):
+        normalized_df = df
+        desig_col_df = normalized_df[desig_col_list]
+        normalized_desig_col_df = (desig_col_df - desig_col_df.mean()) / desig_col_df.std()
+        normalized_df[desig_col_list] = normalized_desig_col_df
+        return normalized_df
+
+    @staticmethod
+    def processDataNormalizationByMaxMin(df, desig_col_list):
+        normalized_df = df
+        desig_col_df = df[desig_col_list]
+        normalized_desig_col_df = (desig_col_df - desig_col_df.min()) / (desig_col_df.max() - desig_col_df.min())
+        normalized_df[desig_col_list] = normalized_desig_col_df
+        return normalized_df
