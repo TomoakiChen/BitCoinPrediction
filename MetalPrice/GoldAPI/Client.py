710e4249331ad12d8b746afaf9d44d4a94f3c308
TomoakiChen <archerugly@gmail.com>
2022-01-02 05:43:12 +0800

    切出更核心的 MetalPriceClient ，各種metal 可以 extends

--- MetalPrice/GoldAPI/Client.py
+++ MetalPrice/GoldAPI/Client.py
@@ -2,7 +2,9 @@ from PyWeb import HttpClient
 from datetime import datetime as DateTime, date as Date, timedelta as TimeDelta
 import pandas as pd
 import json
+import time
 
+# ====================================== 以下算更核心的 Client、Helper ======================================
 class GoldAPIClient(HttpClient):
     __api_key = None
     __base_url = None
@@ -48,24 +50,33 @@ class GoldAPIClient(HttpClient):
         str_price_json = self.getJsonGoldAPIPrice(date, metal_symbol, currency)
         dict_price = json.loads(str_price_json)
         str_date = dict_price["date"]
-        date = GoldPriceHelper.parseStrDate(str_date)
+        date = MetalPriceHelper.parseStrDate(str_date)
         dict_price["date"] = date
         return dict_price
 
-class GoldPriceClient():
+class MetalPriceClient():
 
     __gold_api_client: GoldAPIClient
     __cache_csv_path: str
+    __metal_code: str
+    __wait_sec: int
+    __currency = "USD"
 
-    def __init__(self, api_key, base_url="https://www.goldapi.io/api", cache_csv_path="./GoldPriceCache.csv"):
+    def __init__(self, api_key, metal_code, cache_csv_path, base_url="https://www.goldapi.io/api", wait_sec=1):
         self.__gold_api_client = GoldAPIClient(api_key, base_url=base_url)
         self.__cache_csv_path = cache_csv_path
+        self.__metal_code = metal_code
+        self.__wait_sec = wait_sec
 
     def __tryLoadCacheDateFrame(self):
         if self.__cache_csv_path != None:
-            df_cache = pd.read_csv(self.__cache_csv_path)
-            df_cache = GoldPriceHelper.processGoldPriceDataFrame(df_cache) #保持原有格式，所以不真的 replace --> 202112312039 DataFrame 裡面讓她是處理過的
-            return df_cache
+            try:
+                df_cache = pd.read_csv(self.__cache_csv_path)
+                df_cache = MetalPriceHelper.processGoldPriceDataFrame(df_cache) #保持原有格式，所以不真的 replace --> 202112312039 DataFrame 裡面讓她是處理過的
+                return df_cache
+            except FileNotFoundError:
+                print("No Cache File Now")
+                return None
         else:
             return None
 
@@ -76,7 +87,7 @@ class GoldPriceClient():
         while(True):
             if isinstance(df_cache, type(None)) == False:
             # if df_cache not None: # 這種不行
-                df_cache_4_desig_date =  df_cache[(GoldPriceHelper.processDate4GoldPriceDataFrame(df_cache)["date"] == now_date)]
+                df_cache_4_desig_date =  df_cache[(MetalPriceHelper.processDate4GoldPriceDataFrame(df_cache)["date"] == now_date)]
                 # df_cache_4_desig_date =  df_cache[(df_cache["date"] == str(now_date))]
                 if isinstance(df_cache_4_desig_date, type(None)) == False and df_cache_4_desig_date.empty: # 找不到 cache 資料才需要 search
                     search_date_list.append(now_date)
@@ -111,8 +122,11 @@ class GoldPriceClient():
         need_search_date_list = self.__obtainSearchDateList(since, until, df_cache)
         # print(need_search_date_list)
         for desig_date in need_search_date_list:
-            daily_price = self.__gold_api_client.getDictGoldAPIPrice(date=desig_date)
+            daily_price = self.__gold_api_client.getDictGoldAPIPrice(date=desig_date, metal_symbol=self.__metal_code)
             price_list.append(daily_price)
+            if self.__wait_sec != None:
+                print("wait for : " + str(self.__wait_sec) + "sec")
+                time.sleep(self.__wait_sec)
         return price_list
 
     def getGoldAPIPriceDataFrame(self, since=Date.today(), until=Date.today(), desig_col_list=None):
@@ -122,13 +136,12 @@ class GoldPriceClient():
         if self.__cache_csv_path != None:
             df.to_csv(self.__cache_csv_path, index=False)
 
-        df = GoldPriceHelper.filteringDateRange(df, since, until)
+        df = MetalPriceHelper.filteringDateRange(df, since, until)
         if desig_col_list is None:
             return df
         else:
             return df[desig_col_list]
-
-class GoldPriceHelper:
+class MetalPriceHelper:
     @staticmethod
     def parseStrDate(str_api_date):
         str_api_date = str(str_api_date)
@@ -138,19 +151,19 @@ class GoldPriceHelper:
             return Date.fromisoformat(str_api_date)
     @staticmethod
     def processGoldPriceDataFrame(df_price):
-        df_price = GoldPriceHelper.processDate4GoldPriceDataFrame(df_price)
+        df_price = MetalPriceHelper.processDate4GoldPriceDataFrame(df_price)
         return df_price
 
 
     @staticmethod
     def processDate4GoldPriceDataFrame(df_price):
-        df_price["date"] = GoldPriceHelper.processDateDataFrame(df_price["date"])
+        df_price["date"] = MetalPriceHelper.processDateDataFrame(df_price["date"])
         return df_price
 
     def processDateDataFrame(df_date):
         str_date_list = df_date.values
-        # date_list = [GoldPriceHelper.parseStrDate(str_date) for str_date in str_date_list]
-        date_list = [GoldPriceHelper.parseStrDate(str_date) for str_date in str_date_list]
+        # date_list = [MetalPriceHelper.parseStrDate(str_date) for str_date in str_date_list]
+        date_list = [MetalPriceHelper.parseStrDate(str_date) for str_date in str_date_list]
         df_date = date_list
         return df_date
 
@@ -190,3 +203,18 @@ class GoldPriceHelper:
             if isinstance(until, str):
                 until = Date.fromisoformat(until)
             return df[(df['date'] >= since) & (df['date'] <= until)]
+# ====================================== 以上算更核心的 Client、Helper ======================================
+
+
+class GoldPriceClient(MetalPriceClient):
+    def __init__(self, api_key, base_url="https://www.goldapi.io/api", cache_csv_path="./GoldPriceCache.csv"):
+        super().__init__(api_key, "XAU", cache_csv_path, base_url)
+
+
+class SilverPriceClient(MetalPriceClient):
+    def __init__(self, api_key, base_url="https://www.goldapi.io/api", cache_csv_path="./SilverPriceCache.csv"):
+        super().__init__(api_key, "XAG", cache_csv_path, base_url)
+
+class PlatinumPriceClient(MetalPriceClient):
+    def __init__(self, api_key, base_url="https://www.goldapi.io/api", cache_csv_path="./SilverPriceCache.csv"):
+        super().__init__(api_key, "XPT", cache_csv_path, base_url)
