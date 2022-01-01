e2af9cec9401e0e2a2215a35384fec44ee72f331
TomoakiChen <archerugly@gmail.com>
2022-01-01 23:29:05 +0800

    commit

--- GoldAPIDemo.py
+++ GoldAPIDemo.py
@@ -14,7 +14,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 # api_key = "goldapi-14sk9jtkxqaji16-io"
 api_key = "goldapi-2rdpxtkxtumogz-io"
-since = "2021-01-01"
+since = "2020-01-01"
 until = "2021-12-28"
 gold_price_client = GoldPriceClient(api_key)
 df_price = gold_price_client.getGoldAPIPriceDataFrame(since=since, until=until)
