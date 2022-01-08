a19a2f3caa64939b22a4edd4ed45152cbc7b3c4b
TomoakiChen <archerugly@gmail.com>
2022-01-08 19:39:05 +0800

    Update Helper.py

--- Training/Data/Collector/Helper.py
+++ Training/Data/Collector/Helper.py
@@ -3,8 +3,8 @@ import pandas as pd
 class GoldAPIPriceInfoHelper:
 
     @staticmethod
-    def parse2HourlyDataFrame(df_daily):
+    def parse2HourlyDataFrame(df_daily, replace_columns):
         df_daily.index = pd.to_datetime(df_daily["date"])
         df_hourly = df_daily.resample("H").fillna("pad")
-        df_hourly.rename(columns={"price": "gold-price"}, inplace=True)
+        df_hourly.rename(columns=replace_columns, inplace=True)
         print(df_hourly)
